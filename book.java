2c6c7d0c7fb3a8247ec983d65f670e0626f93a89
girta79 <136323645+girta79@users.noreply.github.com>
2024-06-11 09:18:11 +0300

    Create book.java

---
+++ book.java
@@ -0,0 +1,5 @@
+Easy: Work with String in ArrayList. All the actions should be available for user.
+
+Medium: Work with String User should be able to repeat all the actions infinitely.
+
+Hard: Create a Book class and work with Book object to the ArrayList.
